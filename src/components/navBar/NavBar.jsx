72af3953176056c88138be65c6461bd0a42ad117
“StefanAiroaie” <airoaiestefan@gmail.com>
2024-01-25 14:28:58 +0100

    css update + nav component

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,10 +1,12 @@
+import "navbar.css"
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
     return (<>
-
-        <p className="logo">Drinks&Chill</p>
-        <nav><Link to="/"><p className="nav">Home</p></Link></nav>
+        <div className="navContainer">
+            <p className="logo">Drinks&Chill</p>
+            <nav><Link to="/"><p className="nav">Home</p></Link></nav>
+        </div>
 
 
     </>);
