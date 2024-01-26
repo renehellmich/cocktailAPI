c162ebef4b4218c258eb0dd49be744f0edd51d96
“StefanAiroaie” <airoaiestefan@gmail.com>
2024-01-26 10:40:58 +0100

    updates detailcocktail

--- src/components/cocktailDetails/CocktailDetails.jsx
+++ src/components/cocktailDetails/CocktailDetails.jsx
@@ -1,38 +1,42 @@
 import { useParams } from "react-router-dom";
 import { useContext, useEffect, useState } from "react";
 import { mainContext } from '../../context/mainProvider'
+import './cocktailDetails.css'
 
 const CocktailDetails = () => {
 
     const { title } = useParams();
-    console.log("hei aici este", title);
+    console.log("hier ist title", title);
+
 
     const { drink, setDrink, data } = useContext(mainContext)
 
-    useEffect(() => {
-        const filterdCocktail = data.filter((cocktail) => {
-            return cocktail.strDrink === title.toString()
+    // useEffect(() => {
+    //     const filterdCocktail = data.filter((cocktail) => {
+    //         return cocktail.strDrink === title.toString()
 
-        })
-        setData(filterdCocktail)
-    }, [drink])
+    //     })
+    //     setData(filterdCocktail)
+    // }, [drink])
 
-    console.log(filterdCocktail);
+    // console.log(filterdCocktail);
 
 
 
     return (<>
+        <div className="cocktailDetail">
+            <p>hei heir ist die seite</p>
+            <p>{title}</p>
 
-        <p>hei asta e pagina</p>
-        {drink
+            {/* {drink
             ? (
                 <><img src={filterdCocktail.strDrinkThumb} alt="" />
                     <h2>{filterdCocktail.strDrink}</h2></>)
             : (<p>...loading</p>)
-        }
+        } */}
 
 
-
+        </div>
     </>);
 }
 
